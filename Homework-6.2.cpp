468913c5e936fbd3e4bc174df592ec2b4ece588d
SkeTch <sketch13rus@mail.ru>
2023-11-25 15:49:39 +0300

    Initial commit

---
+++ Homework-6.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int user_numbers;
+    std::cout << "[IN]" << std::endl;
+    std::cin >> user_numbers;
+
+    std::set<int> set_of_numbers;
+    for (int i = 0; i < user_numbers; i++) {
+        int num;
+        std::cin >> num;
+        set_of_numbers.insert(num);
+    }
+
+    std::vector<int> sorted_numbers(set_of_numbers.begin(), set_of_numbers.end());
+    std::sort(sorted_numbers.rbegin(), sorted_numbers.rend());
+
+    std::cout << "[OUT]:" << std::endl;
+    for (const auto& num : sorted_numbers) {
+        std::cout << num << std::endl;
+    }
+
+    return 0;
+}
